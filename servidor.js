a4201251d4308877ee44da442f54dc46b050af9e
Bryan <bryanribero@hotmail.com>
2026-02-24 04:27:34 -0300

    creando una api rest basica

--- servidor.js
+++ servidor.js
@@ -1,8 +1,21 @@
 import http from 'http'
 
 let server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/plain' })
-  res.end('Mensaje de saludo')
+  if (req.method === 'POST' && req.url === '/prueba') {
+    let body = ''
+
+    req.on('data', (chunk) => {
+      body += chunk
+    })
+
+    req.on('end', () => {
+      const bodyString = body.toString()
+      /*  const data = JSON.parse(bodyString) */
+
+      res.writeHead(200, { 'content-type': 'application/json' })
+      res.end(JSON.stringify({ received: bodyString }))
+    })
+  }
 })
 
 server.listen(3000, () => {
